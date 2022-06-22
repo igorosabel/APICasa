c40287ef88da22b6af12126f799099bb2ca2a048
Iñigo <inigo.gorosabel@gmail.com>
2022-06-22 23:01:56 +0200

    Correccion en modulo

--- app/module/api/api.module.php
+++ app/module/api/api.module.php
@@ -5,7 +5,7 @@ namespace OsumiFramework\App\Module;
 use OsumiFramework\OFW\Routing\OModule;
 
 #[OModule(
-	actions: ['checkPasswordToken', 'getMessage', 'getMessages', 'getTags', 'getUser', 'login', 'newPassword', 'recover', 'register', 'saveMessage', 'updatePass', 'updateTask', 'updateUser']
+	actions: ['checkPasswordToken', 'getMessage', 'getMessages', 'getTags', 'getUser', 'login', 'newPassword', 'recover', 'register', 'saveMessage', 'updatePass', 'updateTask', 'updateUser'],
 	type: 'json',
 	prefix: '/api'
 )]
